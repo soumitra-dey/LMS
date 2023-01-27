31aa07f992c5dd423b24754d16f9bc68776ca908
subhankarroy612 <subhankarroy612@gmail.com>
2023-01-27 15:05:58 +0530

    signup ui  created

--- frontend/src/pages/auth/signup.tsx
+++ frontend/src/pages/auth/signup.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
-    Box
+    Box, Button, Text
 } from '@chakra-ui/react';
 import styles from '../../styles/signup.module.css'
 import FormInput from '../../components/FormInput';
 
 const form = [
-    { label: 'Username' },
-    { label: 'Email' },
-    { label: 'Password' }
+    { label: 'Username', type: 'text', name: 'username' },
+    { label: 'Email', type: 'text', name: 'email' },
+    { label: 'Password', type: 'password', name: 'password' }
 ]
 
 export default function signup() {
+
+    const [details, setDetails] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (name: string, value: string) => {
+        setDetails({ ...details, [name]: value })
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(details);
+    }
+
     return (
-        <Box
+        <Box 
             bgImage='linear-gradient(black, white)'
             h='100vh'
             color='black'
@@ -21,12 +37,25 @@ export default function signup() {
             flexDirection='column'
             justifyContent='center'
         >
-            
-            <form id={styles['signup-form']}>
-                {
-                    form.map((ele, i) => <FormInput label={ele.label} />)
-                }
-            </form>
+
+            <Box id={styles['signup-form-parent']}>
+                <Text fontSize='4xl'> Signup  </Text>
+                <Text fontSize='2xl' as='i'> to start learning </Text>
+                <form id={styles['signup-form']} onSubmit={(e) => handleSubmit(e)}>
+                    {
+                        form.map((ele, i) => <FormInput key={i} handleChange={handleChange} label={ele.label} type={ele.type} name={ele.name} />)
+                    }
+                    <Button
+                        type='submit'
+                        id={styles['register-button']}
+                        color='white'
+                        bg='blackAlpha.800'
+                        transition={'0.4s'}
+                        position='absolute'
+                        bottom='0px'
+                    >Register</Button>
+                </form>
+            </Box>
 
         </Box >
     )

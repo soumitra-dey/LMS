12d995447cb7b6a89367f26f549857a6cabe7786
1Gireesh <gireeshbhat68@gmail.com>
2023-02-11 17:50:50 +0530

    completed assaignment form frontend

--- frontend/src/components/ChakraFormInput.tsx
+++ frontend/src/components/ChakraFormInput.tsx
@@ -1,17 +1,18 @@
-import { Button, Flex, Input, Textarea } from '@chakra-ui/react'
+import { Button, Flex, Input, Text, Textarea } from '@chakra-ui/react'
 import React from 'react'
 import styles from '../styles/ChakraFormInput.module.css'
 
 
-export default function ChakraFormInput({ id, handleRemove, handleAssignment }: { id: number, handleRemove: Function, handleAssignment: Function }) {
+export default function ChakraFormInput({ handleRemove, topic, description, index, assignmentSubject }:
+    { handleRemove: Function, topic: string, description: string, index: number, assignmentSubject: string }) {
 
 
 
     return (<Flex flexDirection='column' gap='20px' className={styles.instructor_assignment}>
-        <label>Assignment Topic</label>
-        <Input onChange={(e) => handleAssignment(id, e)} placeholder={'Enter topic'} />
-        <Textarea placeholder='Enter details' />
-        <Button onClick={() => handleRemove()} bg='red' color='white'>Remove</Button>
+        <Text>{topic} </Text>
+        <Text>{assignmentSubject} </Text>
+        <Text>{description} </Text>
+        <Button onClick={() => handleRemove(index)} bg='red' color='white'>Remove</Button>
     </Flex>
     )
 }
